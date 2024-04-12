3a5b613d17ab895f246b6551050074f06dd75191
KimSeokJin6 <veronikakostionova@gmail.com>
2024-04-12 19:23:23 +0300

    Шапка сайта

--- src/js/app.js
+++ src/js/app.js
@@ -16,3 +16,16 @@ const swiper1 = new Swiper(".swiper1", {
 
   // And if we need scrollbar
 });
+
+document.getElementById("burger").addEventListener("click", function () {
+  document.querySelector(".header").classList.toggle("open");
+});
+
+const elements = document.querySelectorAll("input[name=tel]");
+
+elements.forEach((element) => {
+  const maskOptions = {
+    mask: "+{7}(000)000-00-00",
+  };
+  const mask = IMask(element, maskOptions);
+});
